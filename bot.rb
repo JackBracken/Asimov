63a709fadfa36665a318f9130787d56e699b7185
Jack Bracken <contact@jacktrick.com>
2014-09-13 20:32:45 +0100

    Fixed issue with attempting to join channels before Auth finishes

--- bot.rb
+++ bot.rb
@@ -4,54 +4,76 @@ require 'socket'
 require 'yaml'
 require_relative 'irc/parser'
 
-class Asimov
-CONFIG = YAML.load_file('config.yml') unless defined? CONFIG
+module Asimov
 
-  def initialize(server, port)
-    @socket   = TCPSocket.open(server, port)
+  CONFIG = YAML.load_file('config.yml') unless defined? CONFIG
 
-    @parser   = Parser.new
-    @channels = CONFIG['channels']
-    @nick     = CONFIG['nick']
-    
-    say "NICK #{@nick}"
-    say "USER #{@nick} 0 * #{@nick}"
-  end
+  class Bot
+    def initialize(server, port)
+      @socket   = TCPSocket.open(server, port)
 
-  def say msg
-    puts msg
-    @socket.puts msg
-  end
-
-  def say_to_chan chan, msg
-    say "PRIVMSG #{chan} :#{msg}"
-  end
-
-  def run
-    until @socket.eof? do
-      msg = @socket.gets
-      puts msg
-
-      # Connect to channels
+      @parser   = Parser.new
+      @channels = CONFIG['channels']
+      @nick     = CONFIG['nick']
       
-    
-      if msg.match(/^PING :(.*)$/)
-        say "PONG #{$~[1]}"
-        next
+      say "NICK #{@nick}"
+      say "USER #{@nick} 0 * #{@nick}"
+
+      authenticated = false
+
+      until authenticated do
+        msg = @socket.gets
+        puts msg
+
+        if msg.match(/^PING :(.*)$/)
+          say "PONG #{$~[1]}"
+          next
+        end
+
+        if msg.include?("End of \/MOTD command")
+          authenticated = true
+        end
+      end
+      
+      @channels.each do |chan|
+        say "JOIN #{chan['name']}"
+      end
+
+      puts @socket.gets
+    end
+
+    def say msg
+      puts msg
+      @socket.puts msg
+    end
+
+    def say_to_chan chan, msg
+      say "PRIVMSG #{chan} :#{msg}"
+    end
+
+    def run
+      until @socket.eof? do
+        msg = @socket.gets
+        puts msg
+      
+        if msg.match(/^PING :(.*)$/)
+          say "PONG #{$~[1]}"
+          next
+        end
       end
     end
-  end
 
-  def quit
-    @channels.each do |c|
-      say "PART #{c['name']} #{CONFIG['quit_message']}"
+    def quit
+      @channels.each do |c|
+        say "PART #{c['name']} :#{CONFIG['quit_message']}"
+      end
+      say "QUIT"
     end
-    say "QUIT"
-  end
 
+  end
 end
 
-bot = Asimov.new("irc.darklordpotter.net", 6667)
+bot = Asimov::Bot.new("irc.darklordpotter.net", 6667)
 
 trap("INT"){ bot.quit }
 
