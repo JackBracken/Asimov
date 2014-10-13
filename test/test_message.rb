a585eed84fe4dfbf9b5bd842a8380ef50999dbcb
Jack Bracken <contact@jacktrick.com>
2014-10-13 23:59:46 +0100

    Test cases for Message class

--- test/test_message.rb
+++ test/test_message.rb
@@ -1,12 +1,34 @@
 require 'minitest/autorun'
-require 'message'
+require 'irc/message'
 
-class TestMessage
+class TestMessage < Minitest::Test
   def setup
-    @msg = Message.new
+    @privmsg      =  Message.new(':Foo!uid1234@some.host.net PRIVMSG #channel :Test message')
+    @user_action  =  Message.new(':Foo!uid1234@some.host.net PRIVMSG #channel :ACTION test action')
+    @user_mode    =  Message.new(':Foo!uid1234@some.host.net MODE #channel +o Asimov')
   end
 
   def test_parse_prefix
-    assert_match /Foo!uid1234@some.host.net/ @msg.parse(":Foo!uid1234@some.host.net PRIVMSG #channel :Test message")
+    assert_equal 'Foo!uid1234@some.host.net', @privmsg.prefix
+    assert_equal 'Foo!uid1234@some.host.net', @user_action.prefix
+    assert_equal 'Foo!uid1234@some.host.net', @user_mode.prefix
+  end
+
+  def test_parse_trailing
+    skip
+    assert_equal 'Test message', @privmsg.trailing
+    assert_equal 'ACTION test action', @user_action.trailing
+    assert_equal '', @user_mode.trailing
+  end
+
+  def test_parse_command
+    skip
+    assert_equal 'PRIVMSG', @privmsg.command
+    assert_equal 'PRIVMSG', @user_action.command
+    assert_equal 'MODE', @user_mode.command
+  end
+
+  def test_parse_params
+    skip
   end
 end
