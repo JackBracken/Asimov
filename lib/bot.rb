567b9534ddcba968e1deaae99762564441ba8256
Jack Bracken <contact@jacktrick.com>
2014-10-15 14:38:52 +0100

    Bring code within RuboCop style guidelines

--- lib/bot.rb
+++ lib/bot.rb
@@ -2,39 +2,34 @@
 
 require 'socket'
 require 'yaml'
-require_relative 'irc/parser'
 
 module Asimov
-
   CONFIG = YAML.load_file('config.yml') unless defined? CONFIG
 
   class Bot
     def initialize(server, port)
       @socket   = TCPSocket.open(server, port)
 
-      @parser   = Parser.new
       @channels = CONFIG['channels']
       @nick     = CONFIG['nick']
-      
+
       say "NICK #{@nick}"
       say "USER #{@nick} 0 * #{@nick}"
 
       authenticated = false
 
-      until authenticated do
+      until authenticated
         msg = @socket.gets
         puts msg
 
         if msg.match(/^PING :(.*)$/)
-          say "PONG #{$~[1]}"
+          say "PONG #{Regexp.last_match[1]}"
           next
         end
 
-        if msg.include?("End of \/MOTD command")
-          authenticated = true
-        end
+        authenticated = true if msg.include?("End of \/MOTD command")
       end
-      
+
       @channels.each do |chan|
         say "JOIN #{chan['name']}"
       end
@@ -42,22 +37,22 @@ module Asimov
       puts @socket.gets
     end
 
-    def say msg
+    def say(msg)
       puts msg
       @socket.puts msg
     end
 
-    def say_to_chan chan, msg
+    def say_to_chan(chan, msg)
       say "PRIVMSG #{chan} :#{msg}"
     end
 
     def run
-      until @socket.eof? do
+      until @socket.eof?
         msg = @socket.gets
         puts msg
-      
+
         if msg.match(/^PING :(.*)$/)
-          say "PONG #{$~[1]}"
+          say "PONG #{Regexp.last_match[1]}"
           next
         end
       end
@@ -67,14 +62,11 @@ module Asimov
       @channels.each do |c|
         say "PART #{c['name']} :#{CONFIG['quit_message']}"
       end
-      say "QUIT"
+      say 'QUIT'
     end
-
   end
 end
 
-bot = Asimov::Bot.new("irc.darklordpotter.net", 6667)
-
-trap("INT"){ bot.quit }
-
+bot = Asimov::Bot.new('irc.darklordpotter.net', 6667)
+trap('INT') { bot.quit }
 bot.run
--- test/test_message.rb
+++ test/test_message.rb
@@ -1,21 +1,21 @@
-require 'minitest/autorun'
+require 'test_helper'
 require 'irc/message'
 
 class TestMessage < Minitest::Test
   def setup
-    @privmsg      =  Message.new(':Foo!uid1234@some.host.net PRIVMSG #channel :Test message')
-    @user_action  =  Message.new(':Foo!uid1234@some.host.net PRIVMSG #channel :ACTION test action')
-    @user_mode    =  Message.new(':Foo!uid1234@some.host.net MODE #channel +o Asimov')
+    @privmsg      =  Message.new(':Foo!uid@some.host PRIVMSG #chan :Test')
+    @user_action  =  Message.new(':Foo!uid@some.host PRIVMSG #chan :ACTION test action')
+    @user_mode    =  Message.new(':Foo!uid@some.host MODE #chan +o Asimov')
   end
 
   def test_parse_prefix
-    assert_equal 'Foo!uid1234@some.host.net', @privmsg.prefix
-    assert_equal 'Foo!uid1234@some.host.net', @user_action.prefix
-    assert_equal 'Foo!uid1234@some.host.net', @user_mode.prefix
+    assert_equal 'Foo!uid@some.host', @privmsg.prefix
+    assert_equal 'Foo!uid@some.host', @user_action.prefix
+    assert_equal 'Foo!uid@some.host', @user_mode.prefix
   end
 
   def test_parse_trailing
-    assert_equal 'Test message', @privmsg.trailing
+    assert_equal 'Test', @privmsg.trailing
     assert_equal 'ACTION test action', @user_action.trailing
     assert_nil @user_mode.trailing
   end
@@ -27,9 +27,9 @@ class TestMessage < Minitest::Test
   end
 
   def test_parse_params
-    assert_equal '#channel', @privmsg.params[0]
-    assert_equal '#channel', @user_action.params[0]
-    assert_equal '#channel', @user_mode.params[0]
+    assert_equal '#chan', @privmsg.params[0]
+    assert_equal '#chan', @user_action.params[0]
+    assert_equal '#chan', @user_mode.params[0]
     assert_equal '+o',       @user_mode.params[1]
     assert_equal 'Asimov',   @user_mode.params[2]
   end
